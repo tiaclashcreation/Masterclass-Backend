efd5416ed71127711bf60d46b571f0a736a7e14d
Tia Clash Creation <tia@clashcreation.com>
2025-06-01 23:28:21 +0100

    updated CORS

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -3,6 +3,16 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    // Respond to preflight request
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
