06bb161aa23d5c1bedc499c379aa614f3dffcc8c
Tia Clash Creation <tia@clashcreation.com>
2025-06-04 06:37:18 +0100

    updated backedn for stripe auto

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -4,7 +4,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://clashcreation.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
