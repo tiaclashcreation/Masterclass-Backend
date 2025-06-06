1b18382ee3964bae397102ad794ca0ceda785bf6
Tia Clash Creation <tia@clashcreation.com>
2025-06-06 03:01:25 +0100

    testing when code over

--- api/secrets-create-checkout-session.js
+++ api/secrets-create-checkout-session.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
       billing_address_collection: 'required',
       customer_email: customerEmail || undefined,
       customer_creation: 'always',
-      discounts: [{ coupon: 'CLASHSECRET' }], // Always apply the CLASHSECRET coupon
+      discounts: [{ coupon: 'EXPIREDCOUPON' }], // Temporarily use an invalid coupon to test fallback
     };
 
     try {
