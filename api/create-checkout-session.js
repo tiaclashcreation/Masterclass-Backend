d87140142be90d3e8360a2f334081077daa2b4d2
Tia Clash Creation <tia@clashcreation.com>
2025-06-08 11:52:02 +0100

    TCAEXTENDED?

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
       billing_address_collection: 'required',
       customer_email: customerEmail || undefined,
       customer_creation: 'always',
-      discounts: [{ coupon: 'CLASHSECRET' }], // Always apply the CLASHSECRET coupon
+      discounts: [{ coupon: 'TCA4EXTENDED' }], // Always apply the TCA4EXTENDED coupon
     };
 
     try {
