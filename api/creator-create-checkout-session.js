3dfbc9c02183419d2185aeacd27f003c394efe2a
Tia Clash Creation <tia@clashcreation.com>
2025-06-22 16:28:21 +0100

    removed hardcode

--- api/creator-create-checkout-session.js
+++ api/creator-create-checkout-session.js
@@ -44,7 +44,6 @@ export default async function handler(req, res) {
       automatic_tax: { enabled: true },
       tax_id_collection: { enabled: true },
       line_items,
-      discounts: [{ coupon: 'TESTDISCOUNT' }],
       metadata: {
         product: 'Creator',
         payment_type: 'one-time',
